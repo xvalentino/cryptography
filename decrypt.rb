f370fa58be098487aab98099efcac3ac72a4ec48
Valentino Espinoza <xvalentino13@gmail.com>
2014-12-18 23:28:52 -0700

    add decrypt

---
+++ decrypt.rb
@@ -0,0 +1,10 @@
+key = 13
+
+message = "Twqn qn oh nefrem"
+
+decrypted_message = message.tr('zdfseytwqpaboiugjrnmxcklh', 'abcdefghijklmnopqrstuvwxyz')
+if key != 13
+  puts message
+else
+  puts decrypted_message
+end
